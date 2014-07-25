4ce61bbc46c0c9926bf1a98a2087076de0ffb6a2
Ardha Herdianto <ardha2008@gmail.com>
2014-07-25 18:00:42 +0700

    Replace home

--- application/views/content/home.php
+++ application/views/content/home.php
@@ -1,20 +1,55 @@
-<div class="col-md-8 column">
-			<div class="row">
-				<div class="col-md-12">
-					<div class="thumbnail">
-						<img alt="300x200" src="<?= base_url() ?>/theme/img/1.jpg" />
-						<div class="caption">
-							<h3>
-								Thumbnail label
-							</h3>
-							<p>
-								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-							</p>
-							<p>
-								<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
-							</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+<div class="col-md-12 column">
+    <div class="col-md-3">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Tentang
+            </div>
+            
+            <div class="panel-body">
+                <p>Tentang manajemen nilai, penilaian siswa berdasarkan mata pelajaran. Tentunya <mark><strong>banyak kekurangan</strong></mark> pada aplikasi ini. Aplikasi ini untuk <del>dipakai</del> <strong>dikembangkan</strong></p>
+            </div>  
+        </div>      
+    </div>
+    
+    <div class="col-md-3">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Manajemen guru
+            </div>
+            
+            <div class="panel-body">
+                <p>Memanajemen data guru, mulai dari menambahkan (create) , melihat (read), dan hapus (delete)</p>
+            </div>  
+        </div>      
+    </div>
+    
+    <div class="col-md-3">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Manajemen siswa
+            </div>
+            
+            <div class="panel-body">
+                <p>Memanajemen data siswa, mulai dari menambahkan (create) , melihat (read), dan hapus (delete)</p>
+            </div>  
+        </div>      
+    </div>
+    
+    <div class="col-md-3">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Manajemen Mata Pelajaran
+            </div>
+            
+            <div class="panel-body">
+                <p>Memanajemen data mata pelajaran, mulai dari menambahkan (create) , melihat (read), dan hapus (delete)</p>
+            </div>  
+        </div>      
+    </div>
+    
+    <div class="clearfix"></div>
+    <hr />
+    
+    <a href="<?= site_url('login') ?>" class="btn btn-lg btn-block btn-primary">Coba ...</a>			
+</div>
+
